aae48ed71dc769bfd995ad021ff5fb64268658ae
Thijs <1051036@hr.nl>
2025-03-19 11:51:50 +0100

    Added: DNS

--- server/Program.cs
+++ server/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
 
@@ -27,20 +28,60 @@ public class Setting
     public string? ClientIPAddress { get; set; }
 }
 
+public class DNSRecord
+{
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Value { get; set; }
+    public int? Priority { get; set; }
+    public int TTL { get; set; }
+}
+
 
 class ServerUDP
 {
     static string configFile = @"../Setting.json";
+    static string dnsFile = "./DNSrecords.json";
     static string configContent = File.ReadAllText(configFile);
     static Setting? setting = JsonSerializer.Deserialize<Setting>(configContent);
 
     // TODO: [Read the JSON file and return the list of DNSRecords]
+    public static List<DNSRecord> LoadDNSRecords()
+    {
+        if (File.Exists(dnsFile))
+        {
+            string json = File.ReadAllText(dnsFile);
+            return JsonSerializer.Deserialize<List<DNSRecord>>(json) ?? new List<DNSRecord>();
+        }
+        else
+        {
+            Console.WriteLine($"Error: {dnsFile} not found!");
+            return new List<DNSRecord>();
+        }
 
+    }
 
 
 
     public static void start()
     {
+        // DNS Record printing
+        static void DNSRecordPrint()
+        {
+
+            List<DNSRecord> dnsRecords = ServerUDP.LoadDNSRecords();
+            if (dnsRecords.Count == 0)
+            {
+                Console.WriteLine("No DNS records found");
+                return;
+            }
+            foreach (var record in dnsRecords)
+            {
+                Console.WriteLine($"DNS Record: {record.Name} -> {record.Value}");
+            }
+        }
+
+        DNSRecordPrint();
 
 
         // TODO: [Create a socket and endpoints and bind it to the server IP address and port number]
