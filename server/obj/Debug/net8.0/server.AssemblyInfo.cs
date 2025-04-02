4f33947d80c723afd7dabaa172aff560882b77b3
Efroem <134206024+Efroem@users.noreply.github.com>
2025-04-02 10:46:01 +0200

    idk

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3b6716718da6630daf885cde851b853e3a6a6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab97e298853c0d90aad587983ba099b11eb08fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
